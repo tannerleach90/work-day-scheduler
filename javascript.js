501907bb6cd0f0e16fde91c37856bb52119c155a
Tanner Leach <tanner.leach90@gmail.com>
2020-02-12 14:48:18 -0700

    javascript added

---
+++ javascript.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+//onclick to handle text area submission
+
+$(".submit-btn").on("click", function () {
+   
+    var textValue = $(this).siblings('.form-control').val();
+    var hour = $(this).parent().attr("id");
+    console.log("hour: ", hour);
+    console.log("text: ", textValue)
+    //save item in local storage
+    localStorage.setItem(hour, textValue);
+})
+
+//function to update the background color based off of time 
+
+function updateBackground () {
+    //get time with moment.js
+
+
+    //if statements determining if hour is current past or pressent
+
+
+    //if current add class of current check out this link w3schools.com/jquery/jquery_css_classes.asp
+
+
+
+    // if past add class of past 
+
+
+
+    //if future add class of future
+
+}
+
+
+
+//pull values from local storage display to screen
+$("#text-9").val(localStorage.getItem("h-9"));
+$("#text-10").val(localStorage.getItem("h-10"));
+$("#text-11").val(localStorage.getItem("h-11"));
+$("#text-12").val(localStorage.getItem("h-12"));
+$("#text-1").val(localStorage.getItem("h-1"));
+$("#text-2").val(localStorage.getItem("h-2"));
+$("#text-3").val(localStorage.getItem("h-3"));
+$("#text-4").val(localStorage.getItem("h-4"));
+$("#text-5").val(localStorage.getItem("h-5"));
+
+
+
+});
